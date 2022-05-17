2dfc4f0b35a716a77b4aa039260d700e6e1bbde9
Nguyen Tuan <nguyenngoctuan2500@gmail.com>
2022-05-17 11:20:13 +0700

    add services

---
+++ capstone-NguyenTuan/blog-single/blog-single.js
@@ -0,0 +1,83 @@
+function Menu() {
+    var mnEl = document.getElementById("mn");
+    mnEl.style.display = "block";
+}
+function menuClose() {
+    var mnEl = document.getElementById("mn");
+    mnEl.style.display = "none";
+
+}
+// function hide() {
+//     var blackEl = document.getElementById("black")
+//     var bgEl = document.getElementById("bg")
+//     var bg2El = document.getElementById("bg2")
+//     var bg3El = document.getElementById("bg3")
+//     var bg4El = document.getElementById("bg4")
+//     var bg5El = document.getElementById("bg5")
+//     var bg6El = document.getElementById("bg6")
+//     var c1El = document.getElementById("C1")
+//     var c2El = document.getElementById("C2")
+//     var c3El = document.getElementById("C3")
+//     var c4El = document.getElementById("C4")
+//     var c5El = document.getElementById("C5")
+//     var c6El = document.getElementById("C6")
+//     if (blackEl.disabled && bgEl.disabled) {
+//         blackEl.disabled = false
+//         c1El.style.color = "white"
+//         c2El.style.color = "white"
+//         c3El.style.color = "white"
+//         c4El.style.color = "white"
+//         c5El.style.color = "white"
+//         c6El.style.color = "white"
+//         bgEl.disabled = false
+//         bgEl.style.backgroundColor = "#232220"
+//         bgEl.style.color = "white"
+//         bgEl.style.border = "1px solid #383836"
+//         bg2El.style.backgroundColor = "#232220"
+//         bg2El.style.color = "white"
+//         bg2El.style.border = "1px solid #383836"
+//         bg3El.style.backgroundColor = "#232220"
+//         bg3El.style.color = "white"
+//         bg3El.style.border = "1px solid #383836"
+//         bg4El.style.backgroundColor = "#232220"
+//         bg4El.style.color = "white"
+//         bg4El.style.border = "1px solid #383836"
+//         bg5El.style.backgroundColor = "#232220"
+//         bg5El.style.color = "white"
+//         bg5El.style.border = "1px solid #383836"
+//         bg6El.style.backgroundColor = "#232220"
+//         bg6El.style.color = "white"
+//         bg6El.style.border = "1px solid #383836"
+//         blackEl.style.backgroundColor = "#232220"
+//         blackEl.style.color = "white"
+//     } else {
+//         blackEl.disabled = true
+//         bgEl.disabled = true
+//         blackEl.style.backgroundColor = "white"
+//         blackEl.style.color = "black"
+//         bgEl.style.backgroundColor = "white"
+//         bgEl.style.color = "black"
+//         bgEl.style.border = "1px solid #ededed"
+//         bg2El.style.backgroundColor = "white"
+//         bg2El.style.color = "black"
+//         bg2El.style.border = "1px solid #ededed"
+//         bg3El.style.backgroundColor = "white"
+//         bg3El.style.color = "black"
+//         bg3El.style.border = "1px solid #ededed"
+//         bg4El.style.backgroundColor = "white"
+//         bg4El.style.color = "black"
+//         bg4El.style.border = "1px solid #ededed"
+//         bg5El.style.backgroundColor = "white"
+//         bg5El.style.color = "black"
+//         bg5El.style.border = "1px solid #ededed"
+//         bg6El.style.backgroundColor = "white"
+//         bg6El.style.color = "black"
+//         bg6El.style.border = "1px solid #ededed"
+//         c1El.style.color = "black"
+//         c2El.style.color = "black"
+//         c3El.style.color = "black"
+//         c4El.style.color = "black"
+//         c5El.style.color = "black"
+//         c6El.style.color = "black"
+//     }
+// }
