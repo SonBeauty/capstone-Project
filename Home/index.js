4cbf240c80588ddf0b849b88edec119b55c8d0df
SonBeauty <sont9779@gmail.com>
2022-05-19 17:34:54 +0700

    add Home

---
+++ Home/index.js
@@ -0,0 +1,9 @@
+function Menu(){
+    var menuEl = document.getElementById("menuHidden")
+    menuEl.style.display = "block"
+
+}
+function menuClose(){
+    var menuEl =document.getElementById("menuHidden")
+    menuEl.style.display = "none"
+}
